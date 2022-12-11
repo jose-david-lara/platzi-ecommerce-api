b0feab6dfadaa80e66eda884c168ff209953c6b9
Jose Lara <jose@hotmail.com>
2022-12-11 16:23:21 -0300

    CLASE 17
    * Jobs
    * Horizon

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use App\Events\ModelRated;
+use App\Jobs\UpdateLastLogin;
 use App\Listeners\SendEmailModelRatedNotification;
+use Illuminate\Auth\Events\Login;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -23,6 +25,10 @@ class EventServiceProvider extends ServiceProvider
 
         ModelRated::class =>[
             SendEmailModelRatedNotification::class,
+        ],
+
+        Login::class =>[
+            UpdateLastLogin::class,
         ]
     ];
 
